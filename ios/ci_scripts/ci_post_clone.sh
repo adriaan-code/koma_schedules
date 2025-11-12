700fae3b3db26c79100ba5d60e6c2c522d048cb2
Adrian Puczyłowski <developer@ecosense.pl>
2025-11-12 08:43:39 +0100

    Poprawiono script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -6,20 +6,23 @@ echo "--- [CI/CD Flutter Setup - Native] START ---"
 
 # --- Definicja Ścieżek ---
 FLUTTER_HOME="$CI_WORKSPACE_PATH/flutter"
-
-# Ścieżki względne, skoro skrypt jest w ios/ci_scripts/
 REPO_ROOT="../.." # Katalog główny repozytorium
-IOS_DIR="../" # Katalog ios/
 
 echo "Ścieżki robocze:"
 echo "Repozytorium (Root): $REPO_ROOT"
-echo "Katalog iOS: $IOS_DIR"
+# Katalog iOS: ../ (usunięto definicję, ponieważ w kroku 3 używamy względnej ścieżki "ios")
 
-# 1. Klonowanie i instalacja Flutter SDK (OK)
+# 1. Klonowanie i instalacja Flutter SDK (stable) + Dodanie do PATH
 echo "1. Klonowanie i instalacja Flutter SDK (stable)..."
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable "$FLUTTER_HOME"
 export PATH="$PATH:$FLUTTER_HOME/bin"
 flutter --version
+
+# --- NOWY KROK ---
+# 1.5. Wstępne pobieranie artefaktów silnika iOS
+echo "1.5. Pobieranie wstępne artefaktów silnika Flutter dla iOS (flutter precache --ios)..."
+flutter precache --ios # <--- TO JEST KLUCZOWA ZMIANA
+# --------------------
 
 # 2. Pub Get
 echo "2. Uruchamianie 'flutter pub get' w katalogu repozytorium..."
@@ -28,8 +31,6 @@ flutter pub get
 
 # 3. Pod Install
 echo "3. Uruchamianie 'pod install' w folderze iOS..."
-# Jesteśmy w katalogu głównym (np. /Volumes/workspace/repository). 
-# Musimy wejść do folderu ios.
 cd ios 
 xcrun pod install
 
